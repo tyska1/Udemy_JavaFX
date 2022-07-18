1951c8f3ede68142249f18f625f1ea94b29fc9b0
tyska1 <tina2skce@onet.pl>
2022-07-18 09:19:18 +0200

    primaryStage window

---
+++ src/main/java/part3/Main.java
@@ -0,0 +1,16 @@
+package part3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+}
